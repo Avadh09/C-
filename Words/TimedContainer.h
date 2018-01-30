602c85fac47e496d8100bf4fb50406276efbd595
Avadh Boriya <avadh.boriya@gmail.com>
2018-01-29 22:18:44 -0800

    Add files via upload

---
+++ Words/TimedContainer.h
@@ -0,0 +1,24 @@
+#ifndef TIMEDCONTAINER_H_
+#define TIMEDCONTAINER_H_
+
+/**
+ * Base class for an STL container that provides
+ * an elapsed microsecond timer.
+ */
+class TimedContainer
+{
+public:
+    TimedContainer();
+    virtual ~TimedContainer();
+
+    void reset_elapsed_time();
+    long get_elapsed_time() const;
+
+protected:
+    void increment_elapsed_time(const long usec);
+
+private:
+    long elapsed_time;
+};
+
+#endif /* TIMEDCONTAINER_H_ */
